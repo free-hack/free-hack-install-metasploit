50627eac4c92a04637d15f5be215edaefd83cb3c
FREE HACK <62211567+free-hack@users.noreply.github.com>
2020-03-15 21:38:06 +0200

    Add files via upload

---
+++ FREE-HACK-install-metasploit.py
@@ -0,0 +1,110 @@
+
+import os
+os.system ("clear")
+def menu():
+
+    print ("\033[0;32m                               .~%%%%%%%%%%%;~.")
+    print ("\033[0;32m                             /%%%%%%%%%%%%%%;:\ ")
+    print ("\033[0;32m                            /%%%%%%/~\/~\;;;,;,\,")
+    print ("\033[0;32m                           |%%%%%%^\    /;;;;.,.|")
+    print ("\033[0;32m                           |%%%%%%%%%\/;;;;;;.,.|")
+    print ("\033[0;33m                  XX       |%%/~~\%%%%%;;;/~~\;,|       XX")
+    print ("\033[0;33m                XX..X      |%|  o  \%%%;/  o  |.|      X..XX")
+    print ("\033[0;33m              XX.....X     |%%\____/%%%;\____/.,|     X.....XX")
+    print ("\033[0;33m          XXXXX.....XX      \%%%%%%%%%/\;;;;;;,, /      XX.....XXXXX")
+    print ("\033[0;33m        X |......XX%,.@      \%%%%%%/%;\;;;;, /      @@%,XX......| X")
+    print ("\033[0;33m        X |.....X  @@%,.@     |%%%%%%%%;;;;,.|     @@%,.@  X.....| X")
+    print ("\033[0;33m        X  \...X     @@%,.@   |% % % % ; ; ;,|   @@%,.@     X.../  X")
+    print ("\033[0;33m         X@ \.X        @@%,.@                  @@%,.@        X./  @")
+    print ("\033[0;31m          @  X          @@%,.@              @@%,.@          X   @")
+    print ("\033[0;31m        , :@ @X            @@%,.@          @@%,.@            X @@")
+    print ("\033[0;31m           :@@@X             @@%,.@      @@%,.@             @@@@:")
+    print ("\033[0;31m          . :@@@#              @@%.,@  @@%,.@              @@@@:")
+    print ("\033[0;31m            . :;:                @@%.@@%,.@                ;:: : .")
+    print ("\033[0;31m              :                    @@%,.@                   ,.")
+    print ("\033[0;31m              : ,                @@%,.@  @@                :")
+    print ("\033[0;31m                                  @@@  @@@ ")
+    print ("										   ")
+    print ("   										    ")
+    print ("\033[0;32m     			   FREE   < _ >   HACK                      ") 
+    print ("      									  ")
+    print ("\033[0;33m			yuotupe ==>   https://youtu.be/hqeIvjJo5Pg         ")
+    print ("                                                                      ")
+
+loop = True
+
+while loop:
+    menu()
+    print ("                                                                               ")
+    print ("                                                                                ")
+    print ("\033[0;34m  install metasploit [1] ")
+    print ("                                                                               ")
+    print ("                                                                                                                                         ")
+    what = input("\033[0;34m  FREE=>HACK==>  ")
+    if what == "1":
+        print ("\033[0;32m   _____   _____    _____   _____        _   _")
+        print ("\033[0;32m  |  ___| |  _  \  | ____| | ____|      | | | |     /   | /  ___| | | / /")
+        print ("\033[0;32m  | |__   | |_| |  | |__   | |__        | |_| |    / /| | | |     | |/ /")
+        print ("\033[0;32m  |  __|  |  _  /  |  __|  |  __|       |  _  |   / / | | | |     | |\ \ ")
+        print ("\033[0;32m  | |     | | \ \  | |___  | |___       | | | |  / /  | | | |___  | | \ \  ")
+        print ("\033[0;32m  |_|     |_|  \_\ |_____| |_____|      |_| |_| /_/   |_| \_____| |_|  \_\ ")
+        print ("                                                                                     ")
+        Ali = input("Do you want to installs 'metasploit' (y/n): ")
+
+        if Ali == "y":
+            print("[+] Please wait install metasploit  'wait' ") 
+            os.system("apt-get install screenfetch -y")
+            os.system("screenfetch")
+            os.system("apt-get update -y")
+            os.system("apt-get upgrade")
+            os.system("apt-get install python -y")
+            os.system("apt-get install python2 -y")
+            os.system("apt-get install git -y     ")
+            os.system("apt-get install php -y")
+            os.system("apt-get install wget -y")
+            os.system("apt-get install curl -y")
+            os.system("apt-get install openssh -y")
+            os.system("apt-get install ruby -y")
+            os.system("apt-get gem install bundle -y")
+            os.system("apt-get pip install bundle -y")
+            os.system("apt-get install curl -y")
+            os.system("git clone https://github.com/rapid7/metasploit-framework.git -y")
+            os.system("curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.s  -y")
+            os.system("apt-get install ruby -y")
+            os.system("apt-get install wget -y")
+            os.system("apt-get install perl -y")
+            os.system("apt-get install curl -y")
+            os.system("apt-geg install proot -y")
+            os.system("wget https://Auxilus.github.io/metasploit.sh")
+            os.system("bash metasploit.sh")
+            os.system("ls")
+            os.system("cd metasploit-framework")
+            os.system("apt-get install wget -y")
+            os.system("gem install bundle -y")
+            os.system("gem install bundler -y")
+            os.system("pip2 install bundler -y")
+            os.system("bundle config build.nokogiri --use-system-libraries")
+            os.system("apt-get install bundle install -y")
+            os.system("apt-get bundle update nokogiri -y")
+            os.system("apt-get install ruby -y")
+            os.system("gem install nokogiri -y")
+            os.system("gem install nokogiri -- --use-system-libraries ")
+            os.system('gem install pkg-config -v "~> 1.1"')
+            os.system("apt-get pkg-config -y")
+            os.system("bundle update nokogiri ")
+            os.system("pg_ctl -D $PREFIX/var/lib/postgresql start")
+            os.system("clear")
+
+            fgf = input ("\033[0;30m [+]  To reinstall me press( y/n) ")
+
+            if fgf == "y":
+
+                menu()
+            else:
+                break
+    elif what == "00":
+        print("FREE HACK xx ")
+        break
+
+
+ 
